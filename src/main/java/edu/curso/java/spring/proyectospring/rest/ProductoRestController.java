6e0d8b9fe74d94bc573c304ad672ca735f2ab33f
claudiozam <claudio.zanoszczyk@honou.com.ar>
2022-06-21 15:47:24 -0300

    ejemplo basico de api rest

--- src/main/java/edu/curso/java/spring/proyectospring/rest/ProductoRestController.java
+++ src/main/java/edu/curso/java/spring/proyectospring/rest/ProductoRestController.java
@@ -3,9 +3,16 @@ package edu.curso.java.spring.proyectospring.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.curso.java.spring.proyectospring.rest.dto.ProductoDTO;
@@ -22,7 +29,7 @@ public class ProductoRestController {
 		productoDTO1.setPrecio(4500.0);
 		
 		ProductoDTO productoDTO2 = new ProductoDTO();
-		productoDTO2.setId(1234L);
+		productoDTO2.setId(4423L);
 		productoDTO2.setNombre("Ejemplo 1234");
 		productoDTO2.setPrecio(4500.0);
 		
@@ -34,12 +41,34 @@ public class ProductoRestController {
 
 	
 	@GetMapping("/productos/{id}")
-	public ProductoDTO recuperarProductoPorId(@PathVariable Long id) {
+	public ResponseEntity<ProductoDTO> recuperarProductoPorId(@PathVariable Long id) {
 		ProductoDTO productoDTO = new ProductoDTO();
 		productoDTO.setId(1234L);
 		productoDTO.setNombre("Ejemplo 1234");
 		productoDTO.setPrecio(4500.0);
-		return productoDTO;
+		
+		if(id == 99) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.ok(productoDTO);
 	}
+	
+	@PostMapping("/productos")
+	public ResponseEntity<ProductoDTO> altaDeNuevoProducto(@RequestBody ProductoDTO productoDTO) {
+
+		return new ResponseEntity(productoDTO, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/productos/{id}")
+	public void actualizarProductoPor(@PathVariable Long id, @RequestBody ProductoDTO productoDTO) {
+		
+	}
+	
+	@DeleteMapping("/productos/{id}")
+	public ResponseEntity<?> borrarProductoPorId(@PathVariable Long id) {
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
+	
+	
 	
 }
