545a298ee63a8e5d19af1dc5bcfa50c76f7a97e7
claudiozam <claudio.zanoszczyk@honou.com.ar>
2022-06-28 14:42:32 -0300

    ejemplo de mvc

--- src/main/java/edu/curso/java/spring/proyectospring/ProyectoSpringApplication.java
+++ src/main/java/edu/curso/java/spring/proyectospring/ProyectoSpringApplication.java
@@ -2,10 +2,17 @@ package edu.curso.java.spring.proyectospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProyectoSpringApplication {
+public class ProyectoSpringApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProyectoSpringApplication.class);
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringApplication.class, args);
 	}
---
+++ src/main/java/edu/curso/java/spring/proyectospring/mvc/HolaMundoController.java
@@ -0,0 +1,41 @@
+package edu.curso.java.spring.proyectospring.mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/holamundo")
+public class HolaMundoController {
+
+	private static  Logger log = LoggerFactory.getLogger(HolaMundoController.class);
+
+	@GetMapping("/saludar")
+	public String saludar(Model model) {
+		log.info("ejecutando saludar...");
+		model.addAttribute("texto", "hola a todos esto es un parametro que mando desde el controller");
+		return "holamundo";
+	}
+	
+	@GetMapping("/saludar2/{texto}")
+	public String saludar2(Model model, @PathVariable String texto) {
+		log.info("ejecutando saludar2...");
+		model.addAttribute("texto", texto);
+		return "holamundo";
+	}
+	
+	@GetMapping("/saludar3")
+	public String saludar3(Model model, @RequestParam String texto) {
+		log.info("ejecutando saludar3...");
+		model.addAttribute("texto", texto);
+		return "holamundo";
+	}
+	
+	
+}
